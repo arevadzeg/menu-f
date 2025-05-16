35ae17e4163f7f8304be09351ba4acca7d7098a6
arevadzeg <gioarevadze97@gmail.com>
2025-05-16 23:59:52 +0400

    corrected scrolling

--- src/app/components/ui/Carousel/Carousel.tsx
+++ src/app/components/ui/Carousel/Carousel.tsx
@@ -1,53 +1,99 @@
-import React from 'react'
-import { EmblaOptionsType } from 'embla-carousel'
-import useEmblaCarousel from 'embla-carousel-react'
-import MainCategoriesCard from "../../product/MainCategories/Components/MainCategoriesDroppableCard/MainCategoriesDroppabeCard";
+import React from 'react';
+import { EmblaOptionsType } from 'embla-carousel';
+import useEmblaCarousel from 'embla-carousel-react';
+import MainCategoriesCard from '../../product/MainCategories/Components/MainCategoriesDroppableCard/MainCategoriesDroppabeCard';
 import { useParams, useRouter } from 'next/navigation';
+import { ChevronRightIcon, ChevronLeftIcon } from "@radix-ui/react-icons"
 
 
 type PropType = {
-    slides: any[]
-    options?: EmblaOptionsType
-}
+    slides: any[];
+    options?: EmblaOptionsType;
+};
 
-const Carousel: React.FC<PropType> = (props) => {
-    const { slides, options } = props
-    const [emblaRef, emblaApi] = useEmblaCarousel({
-
-    })
-
+const Carousel: React.FC<PropType> = ({ slides, options }) => {
+    const [emblaRef, emblaApi] = useEmblaCarousel(options);
     const { appName, categoryId } = useParams();
     const router = useRouter();
-
 
     const handleNavigateToCategory = (categoryId: string) => {
         router.push(`/${appName}/${categoryId}`);
     };
 
 
-    console.log('slides', slides)
+    const scrollPrev = () => {
+        if (!emblaApi) return;
+        const currentIndex = emblaApi.selectedScrollSnap();
+        const targetIndex = Math.max(currentIndex - 2, 0);
+        emblaApi.scrollTo(targetIndex);
+    };
+    const scrollNext = () => {
+        if (!emblaApi) return;
+        const currentIndex = emblaApi.selectedScrollSnap();
+        const maxIndex = emblaApi.scrollSnapList().length - 1;
+        const targetIndex = Math.min(currentIndex + 2, maxIndex);
+        emblaApi.scrollTo(targetIndex);
+    };
 
+    // TODO PROBABLY NEED TO REMOVE THIS SHIT
     return (
-        <section className="embla">
+        <section className="embla relative">
+            <button
+                style={{
+                    position: "absolute",
+                    left: "-40px",
+                    cursor: "pointer",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: "30px",
+                    height: "30px",
+                    border: "1px solid #ebebeb",
+                    borderRadius: 8
+
+                }}
+                className="left-2 top-1/2 transform -translate-y-1/2"
+                onClick={scrollPrev}
+            >
+                <ChevronLeftIcon />
+
+            </button>
             <div className="embla__viewport" ref={emblaRef}>
                 <div className="embla__container">
                     {slides.map((index) => (
                         <div className="embla__slide" key={index.id}>
-
-                            <div className="embla__slide__number">
-
-                                <MainCategoriesCard
-                                    category={index}
-                                    isSelected={categoryId === index.id}
-                                    handleNavigateToCategory={handleNavigateToCategory}
-                                />
-                            </div>
+                            <MainCategoriesCard
+                                category={index}
+                                isSelected={categoryId === index.id}
+                                handleNavigateToCategory={handleNavigateToCategory}
+                            />
                         </div>
                     ))}
                 </div>
             </div>
-        </section>
-    )
-}
+            <button
+                style={{
+                    position: "absolute",
+                    right: "-40px",
+                    cursor: "pointer",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: "30px",
+                    height: "30px",
+                    border: "1px solid #ebebeb",
+                    borderRadius: 8
 
-export default Carousel
+                }}
+                className="absolute right-2 top-1/2 transform -translate-y-1/2"
+                onClick={scrollNext}
+            >
+                <ChevronRightIcon />
+            </button>
+        </section>
+    );
+};
+
+export default Carousel;
+
+
